1afdd15ca64b086b8cae472e777f31350f5d4b43
QUANTUM-PHOENIX04 <119916710+QUANTUM-PHOENIX04@users.noreply.github.com>
2022-12-07 10:54:08 +0530

    Delete N IS PRIME.py

--- N IS PRIME.py
+++
@@ -1,9 +0,0 @@
-N=int(input("ENTER A NATURAL NO: "))
-
-for I in (2,N):
-    if N%I==0:
-        print("IT IS NOT A PRIME NO")
-        break
-    else:
-        print("IT IS A PRIME NO") 
-        break   
