77e48c8525f20b31a144f5e993256adb2eeff87b
QUANTUM-PHOENIX04 <119916710+QUANTUM-PHOENIX04@users.noreply.github.com>
2023-01-01 18:04:44 +0530

    Add files via upload

---
+++ P3.py
@@ -0,0 +1,15 @@
+
+while 1 == True:
+    n= int(input("enter the number of rows:"))  
+    i= int(1)
+    while n != 0:
+        print("* "*i,"/n")
+        i += 2
+        n -= 1          
+
+
+while 1 == True:
+    n= int(input("enter the number of rows:"))
+    while n !=0:
+        print("* "*(2*n - 1))
+        n=1
